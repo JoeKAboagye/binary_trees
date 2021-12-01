23605e5ace6c07211c48009a195c8d73e632577c
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2021-12-01 23:37:26 +0000

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,7 +5,6 @@
  * @tree: root node of the tree to traverse
  * @func: pointer to a function to call for each node, for formatted printing
  *
- * Return: void
  */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
